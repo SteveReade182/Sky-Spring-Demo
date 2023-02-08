47019d20d737ed114df54cd1d931029edb6c42e5
SteveReade182 <sjreade13@outlook.com>
2023-02-08 11:41:08 +0000

    added entities package with Person class. added rest package with PersonController class.

---
+++ src/main/java/com/sky/people/entities/Person.java
@@ -0,0 +1,52 @@
+package com.sky.people.entities;
+
+public class Person {
+
+    // variables
+
+    private String name;
+
+    private int age;
+
+    private String job;
+
+    // constructors
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    // REQUIRED must have a default constructor and getters and setters for Spring to work!
+    public Person(){
+    }
+
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+
+}
