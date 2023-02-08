e88e8d539295880600391b7cd8fba1d00bac13fd
SteveReade182 <sjreade13@outlook.com>
2023-02-08 14:37:42 +0000

    finished controller

--- src/main/java/com/sky/people/PeopleApplication.java
+++ src/main/java/com/sky/people/PeopleApplication.java
@@ -10,4 +10,6 @@ public class PeopleApplication {
 		SpringApplication.run(PeopleApplication.class, args);
 	}
 
+
+
 }
--- src/main/java/com/sky/people/rest/PersonController.java
+++ src/main/java/com/sky/people/rest/PersonController.java
@@ -1,17 +1,53 @@
 package com.sky.people.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.sky.people.entities.Person;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+    private List<Person> people = new ArrayList<>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String greeting(){
         return "Hello, Steve!";
     }
 
+    @PostMapping("/create")
+    public Person addPerson(@RequestBody Person person) {  // pulls person from the body of the request
+        this.people.add(person); // add a new person to the list
+        return this.people.get(this.people.size() -1); // returns the last person in the list
+    }
+
+    @GetMapping("/getAll")
+    public List<Person> getAll(){
+        return this.people;
+    }
+
+    @GetMapping("/get/{id}") // pulls id from the path (url)
+    public Person getPerson(@PathVariable int id){
+        return this.people.get(id);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Person removePerson(@PathVariable int id){
+        return this.people.remove(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public Person updatePerson(@PathVariable int id, @PathParam("name") String name,@PathParam("age") Integer age,@PathParam("job") String job){
+        Person old = this.people.get(id);
+
+        if (name != null) old.setName(name);
+        if (age != null) old.setAge(age);
+        if (job != null) old.setJob(job);
+
+        return old;
+    }
 
 
 }
