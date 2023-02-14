ba9fde52b919e723e55b7b402b1d5b090b32d0e4
SteveReade182 <sjreade13@outlook.com>
2023-02-14 12:05:02 +0000

    writing pid file added

--- src/main/java/com/sky/people/PeopleApplication.java
+++ src/main/java/com/sky/people/PeopleApplication.java
@@ -2,12 +2,19 @@ package com.sky.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class PeopleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeopleApplication.class, args);
+//		SpringApplication.run(PeopleApplication.class, args);
+
+		SpringApplication application =
+				new SpringApplication(PeopleApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+
 	}
 
 }
--- src/test/java/com/sky/people/rest/PersonIntegrationTests.java
+++ src/test/java/com/sky/people/rest/PersonIntegrationTests.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // loads the app context with all your beans
 @AutoConfigureMockMvc // sets up the testing library
+@Sql(scripts = {"classpath:person-schema.sql","classpath:person-data.sql"})
 public class PersonIntegrationTests {
 
     @Autowired // tells Spring to inject the mvc object into my test class.
@@ -31,16 +35,66 @@ public class PersonIntegrationTests {
 
         RequestBuilder req = MockMvcRequestBuilders.post("/create").content(newPersonAsJSON).contentType(MediaType.APPLICATION_JSON);
 
+        //check the response status
         ResultMatcher checkStatus = MockMvcResultMatchers.status().isCreated();
 
         PersonDTO created = new PersonDTO("Steve",32,"Coder");
 
+        //check the response body
         ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(created));
+
+        //do the request and check the response
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+    }
+
+    @Test
+    void testGet() throws Exception{
+
+        RequestBuilder req = MockMvcRequestBuilders.get("/get/1");
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("JB",24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
 
         this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
 
     }
 
+    @Test
+    void testGetAll() throws Exception{
 
+        RequestBuilder req = MockMvcRequestBuilders.get("/getAll");
 
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("JB",24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(List.of(person)));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+    }
+
+    @Test
+    void testUpdate() throws Exception{
+        RequestBuilder req = MockMvcRequestBuilders.patch("/update/1").queryParam("name","Ant").queryParam("age","30").queryParam("job","Magic Man");
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+        PersonDTO person = new PersonDTO("Ant",30, "Magic Man");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testRemove() throws Exception{
+        RequestBuilder req = MockMvcRequestBuilders.delete("/remove/1").contentType(MediaType.APPLICATION_JSON);
+
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+
+        PersonDTO person = new PersonDTO("JB",24, "Trainer");
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(person));
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+    }
 }
